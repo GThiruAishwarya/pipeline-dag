8fa940d0247947fc941707f05c31250a58288c33
GThiruAishwarya <167677392+GThiruAishwarya@users.noreply.github.com>
2025-07-03 10:50:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  root: '.', // ensure root is current directory
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  publicDir: 'public', // tell Vite where public/index.html is
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
 });
